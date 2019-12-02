f8d0f0285c6271c2adf4bbb26325e05081e385b4
brandon-b-miller <brmiller@nvidia.com>
2019-12-02 08:08:02 -0800

    add numeric cases where indices don't match

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -4112,12 +4112,19 @@ def test_cov_nans():
     assert_eq(pdf.cov(), gdf.cov())
 
 
-@pytest.mark.parametrize("sr_data", [[1,2,3]])
-def test_df_sr_binop(sr_data):
-    gdf = DataFrame({0: [0,0,0], 1:[0,0,0], 2:[0,0,0]})
+@pytest.mark.parametrize(
+    "gsr",
+    [
+        Series([1, 2, 3]),
+        Series([1, 2, 3], index=["a", "b", "c"]),
+        Series([1, 2, 3], index=["a", "b", "d"]),
+        Series([1, 2], index=["a", "b"]),
+    ],
+)
+def test_df_sr_binop(gsr):
+    gdf = DataFrame({"a": [0, 0, 0], "b": [0, 0, 0], "c": [0, 0, 0]})
     pdf = gdf.to_pandas()
 
-    gsr = Series(sr_data)
     psr = gsr.to_pandas()
 
     expect = pdf + psr
@@ -4135,4 +4142,3 @@ def test_df_sr_binop(sr_data):
     expect = pdf / psr
     got = gdf / gsr
     assert_eq(expect, got)
-
